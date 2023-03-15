ed179d8552aa22a190646f4d8523430778b5195c
Liam McCartney <liam.mccartney@gmail.com>
2023-03-15 14:16:02 -0400

    PONG

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{net::{TcpListener}, io::Write};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                _stream.write(b"+PONG\r\n");
+                _stream.flush();
             }
             Err(e) => {
                 println!("error: {}", e);
