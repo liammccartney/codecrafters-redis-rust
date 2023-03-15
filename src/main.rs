768e2c4efbc1d85bfdc0769d23154a5fbd7b3d5a
Liam McCartney <liam.mccartney@gmail.com>
2023-03-15 14:20:53 -0400

    Refactor based on verified solution

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 // Uncomment this block to pass the first stage
-use std::{net::{TcpListener}, io::Write};
+use std::{net::TcpListener, io::{Write, Read}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                _stream.write(b"+PONG\r\n");
-                _stream.flush();
+            Ok(mut stream) => {
+                let mut buf = [10; 512];
+                stream.read(&mut buf).unwrap();
+                stream.write(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
